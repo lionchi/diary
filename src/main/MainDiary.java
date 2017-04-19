8ed874e560fe3257790d7a8e4b683e185ae554fc
TanyaBelova <tanya.belova19@yandex.ru>
2017-04-19 22:28:06 +0300

    Добавила переменную

--- src/main/MainDiary.java
+++ src/main/MainDiary.java
@@ -21,6 +21,7 @@ import java.util.List;
 public class MainDiary extends Application {
 
     private Stage primaryStage;
+    private  String s;
 
     public static void main(String[] args) throws SQLException {
 
